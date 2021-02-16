4dc709f4662814b35e992a5354e4d02e7a61a81a
Meruyert Karim <meruyert.karim@nu.edu.kz>
2021-02-17 00:00:02 +0100

    initial sass architecure

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Home from "./pages/Home";
+import './styles/style.css';
+
 
 function App() {
     return (
