ba771e7e47fe8c88652e00436a6c0aff7996a0db
Meruyert Karim <meruyert.karim@nu.edu.kz>
2021-03-07 23:59:16 +0100

    profile images

--- src/data/dataPeople.js
+++ src/data/dataPeople.js
@@ -1,37 +1,37 @@
 const people = [
     {
     id: 1,
-    image:'/images/profilePhotos/profile.jpg',
+    image:'/images/profilePhotos/profile1.jpg',
     name: 'Ingrid Adam',
     position: "Product Manager",
     },
     {
     id: 2,
-    image: '/images/profilePhotos/profile.jpg',
+    image: '/images/profilePhotos/profile2.jpg',
     name: 'Yuvin Ha',
     position: "Designer",
     },
     {
     id: 3,
-    image: '/images/profilePhotos/profile.jpg',
+    image: '/images/profilePhotos/profile3.jpg',
     name: 'Michelle Torres-Linke',
     position: "Agile Coach"
     },
     {
     id: 4,
-    image: '/images/profilePhotos/profile.jpg',
+    image: '/images/profilePhotos/profile4.jpg',
     name: 'Meruyert Karim',
     position: "Software Engineer"
     },
     {
     id: 5,
-    image: '/images/profilePhotos/profile.jpg',
+    image: '/images/profilePhotos/profile5.jpg',
     name: 'Aleksei Nepochatykh',
     position: "Software Engineer"
     },
     {
     id: 6,
-    image: '/images/profilePhotos/profile.jpg',
+    image: '/images/profilePhotos/profile6.jpg',
     name: 'Goran Ivkovic Ivandekic',
     position: "AI Engineer"
     },
