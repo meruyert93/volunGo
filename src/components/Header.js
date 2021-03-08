00e2ea467d9258bb4de6c7d946cf6bedb41940e2
Meruyert Karim <meruyert.karim@nu.edu.kz>
2021-03-08 11:59:00 +0100

    logo and mockup image

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,11 +8,11 @@ function HeaderBar({onToggleMenu}) {
             <Grid container columns={3}>
                 <Grid.Row>
                     <Grid.Column floated='left' width={3}>
-                        <Image src='./images/Logomark.svg' />
+                        <Image src='./images/Logo.png' />
                     </Grid.Column>
-                    <Grid.Column floated='left' width={11}>
+                    {/* <Grid.Column floated='left' width={11}>
                         <Header as='h1'>VolunGO</Header>
-                    </Grid.Column>
+                    </Grid.Column> */}
                     <Grid.Column floated='right' width={2}>
                         <Icon 
                             link 
