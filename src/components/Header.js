2abf4d6b463325e1aadefa8c7772192bf1ed49d7
Meruyert Karim <meruyert.karim@nu.edu.kz>
2021-03-08 12:43:24 +0100

    logo path

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,22 @@
 import React from 'react';
 import { Segment, Header, Grid, Icon, Image } from 'semantic-ui-react';
+import { useHistory } from "react-router-dom";
 
 function HeaderBar({onToggleMenu}) {
+    
+    const RouteChangeBack = () => {
+        let path = `/`;
+        history.push(path);
+    }
+
+    const history = useHistory();
     
     return (
         <Segment basic attached="top" vertical compact size='small'>
             <Grid container columns={3}>
                 <Grid.Row>
                     <Grid.Column floated='left' mobile={8} tablet={4} computer={3}>
-                        <Image src='./images/Logo.png' />
+                        <Image src='./images/Logo.png' href='https://volungo.netlify.app/' />
                     </Grid.Column>
                     {/* <Grid.Column floated='left' width={11}>
                         <Header as='h1'>VolunGO</Header>
