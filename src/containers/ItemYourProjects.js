3bcbd0f683189b92b66417c89dff9a51ecc892a6
Yuvin Ha <yuvinha@Yuvins-MacBook-Pro.local>
2021-03-22 21:03:18 +0100

    Fix the style of added projects on volunteer side

--- src/containers/ItemYourProjects.js
+++ src/containers/ItemYourProjects.js
@@ -1,25 +1,48 @@
 import React, { useState } from 'react';
 import { Grid, Image, Card, Icon, Button } from 'semantic-ui-react';
-import { useTranslation } from "react-i18next";
+import { useTranslation } from 'react-i18next';
 
-function ItemYourProjects({id, image, name, organization, duration, days, time, place, displayDetailedProject }) {
-    const [active, setActive] = useState();
+function ItemYourProjects({
+  id,
+  image,
+  name,
+  organization,
+  duration,
+  days,
+  time,
+  place,
+  displayDetailedProject,
+}) {
+  const [active, setActive] = useState();
 
-    const { t } = useTranslation();
+  const { t } = useTranslation();
 
-    return (
-        <Grid.Column mobile={16} tablet={5} computer={5} stretched>
-            <Card  onClick={() => {}} className="projectCard">
-                <Card.Content>
-                    <Card.Header className="cardHeadingText">{name} </Card.Header>
-                    <p className="textPrimary smallText">{organization}</p>
-                </Card.Content>
-                <Card.Content>
-                    <Button color='yellow'>{t('pending')}</Button>
-                </Card.Content>
-            </Card>
-        </Grid.Column>
-    )
+  return (
+    <Grid.Column mobile={16} tablet={5} computer={5} stretched>
+      <Card
+        onClick={() => {}}
+        className="projectCard"
+        style={{ padding: '8px 12px 4px 12px' }}
+      >
+        <Card.Content>
+          <Card.Header
+            className="cardHeadingText card-title"
+            style={{ marginBottom: '8px' }}
+          >
+            {name}{' '}
+          </Card.Header>
+          <p className="textPrimary smallText card-organization">
+            {organization}
+          </p>
+        </Card.Content>
+        <Card.Content>
+          <Button color="yellow" className="tag">
+            {t('pending')}
+          </Button>
+        </Card.Content>
+      </Card>
+    </Grid.Column>
+  );
 }
 
 export default ItemYourProjects;
